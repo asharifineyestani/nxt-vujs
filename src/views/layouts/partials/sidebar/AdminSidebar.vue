772579fbbc2c4f83b669ad903d2a6d5a1337b881
Ali Sharifi Neyestani <sh4rifi@gmail.com>
2020-05-25 12:31:33 +0430

    first commit

---
+++ src/views/layouts/partials/sidebar/AdminSidebar.vue
@@ -0,0 +1,52 @@
+<template>
+  <ul>
+  </ul>
+</template>
+
+
+<script>
+  import {employerRoutes} from './EmployerRoutes'
+  import {candidateRoutes} from './CandidateRoutes'
+  import Sh4SidebarLink from './components/Sh4SidebarLink'
+
+  export default {
+    name: 'admin-sidebar',
+    components: {
+      Sh4SidebarLink,
+    },
+    props: {
+      minimized: {
+        type: Boolean,
+        required: true,
+      },
+    },
+    data() {
+      return {}
+    },
+
+    created() {
+      let current =  this.$route.path;
+      let segments = current.split("/");
+      console.log(segments[2])
+
+      },
+
+    computed: {
+      // a computed getter
+      items: function () {
+        let current =  this.$route.path;
+        let segments = current.split("/");
+        if (segments[2] == 'candidate')
+          return candidateRoutes.routes
+         else
+          return employerRoutes.routes
+      }
+    }
+  }
+
+</script>
+
+
+<style lang="scss">
+
+</style>
