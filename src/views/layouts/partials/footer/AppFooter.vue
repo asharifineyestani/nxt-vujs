9243d8c4a3941bded202b35246be173987b84bd5
Ali Sharifi Neyestani <sh4rifi@gmail.com>
2020-06-01 01:49:00 +0430

    minor modify

---
+++ src/views/layouts/partials/footer/AppFooter.vue
@@ -0,0 +1,17 @@
+<template>
+</template>
+
+
+<script>
+  export default {
+    name: 'app-footer',
+    props: {},
+    data() {
+      return
+    }
+  }
+</script>
+
+
+<style lang="scss">
+</style>
---
+++ src/views/sections/Section2.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>  Section2</div>
+</template>
+
+
+<script>
+
+
+  export default {
+    name: 'Section2',
+    components: {},
+    props: {},
+    data() {
+      return {}
+    },
+  }
+
+</script>
+
+
